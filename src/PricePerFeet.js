e1918889824e8eb73dcd9b933682e75debd426c0
mike mendes <4mikemendes@gmail.com>
2021-11-05 08:06:14 +0100

    style

--- src/PricePerFeet.js
+++ src/PricePerFeet.js
@@ -40,6 +40,7 @@ class PricePerFeet extends Component {
         })
     }
 
+
     render() {
        
         return(
@@ -48,26 +49,34 @@ class PricePerFeet extends Component {
             <h1 className="title">Boat price per feet</h1>
             <label className="label">Boat specs</label>
             <form className="form">
-                <input type="text" placeholder={"boat name"} name="name"
+                <input className="input" type="text" placeholder={"boat name"} name="name"
                 onChange={this.handelChange}/>
                 <br/>
-                <input type="text" placeholder={"Brand"} name="brand"
+                <input className="input" type="text" placeholder={"Brand"} name="brand"
                 onChange={this.handelChange}/>
                 <br/>
-                <input type="number" placeholder={"feet"} name="feet" math="math"
+                <input className="input" type="number" placeholder={"feet"} name="feet" math="math"
                 onChange={this.handelChange} />
                 <br/>
-                <input type="number" placeholder={"days"} name="days" total="total"
+                <input className="input" type="number" placeholder={"days"} name="days" total="total"
                 onChange={this.handelChange} />
                 <br/>
+                <div className="checkbox--container">
+                <input className="input" type="checkbox" name="powerWash"></input>
+                <label className="label">Power wash</label>
+                <input className="input" type="checkbox" name="mast"></input>
+                <label className="label">Lift Mast</label>
+                </div>
                 <button className="button" onClick={this.total} >Submit</button>
+                <div><h1 className="total">Total: {this.state.total}</h1></div>
+            </form>
+            {/* <h2>Summary</h2>
             <h3> Name: {this.state.name} </h3>
             <h3> Brand: {this.state.brand} </h3>
             <h3> Length: {this.state.feet} </h3>
             <h3>Days: {this.state.days}</h3>
-            <h3>Total: {this.state.total}</h3>
-            </form>
-            {/* <h4> Price per feet per day is: 0.20726399 eur  </h4> */}
+            <h3>Total: {this.state.total} €</h3>
+            <h4> Price per feet per day is: 0.20726399 eur  </h4> */}
         </div>
     )
     }
